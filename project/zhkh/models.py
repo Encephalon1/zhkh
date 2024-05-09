76a9f69353eb01a0380fe7c38145c995cc73f438
Encephalon1 <viizdevaetes@mail.ru>
2024-05-09 11:40:19 +0300

    'models'

--- project/zhkh/models.py
+++ project/zhkh/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Announcement(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+
+class TenantRequest(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+
+
+class ResponceToRequest(models.Model):
+    ten_request = models.OneToOneField(TenantRequest, on_delete=models.CASCADE)
+    text = models.TextField()
