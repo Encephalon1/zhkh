a62b6dea1fd79362b177c16eacfeff639ff57bca
Encephalon1 <viizdevaetes@mail.ru>
2024-09-23 17:55:51 +0300

    bugs_router

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Navigate, Route, Routes } from "react-router-dom";
-import { Header } from "./components/header/Header"
+import Header from "./components/header/Header"
 
 function App() {
   return (
