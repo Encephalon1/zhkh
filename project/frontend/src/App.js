ecaecd54cf215fd286d57b30738085de27b20d13
Encephalon1 <viizdevaetes@mail.ru>
2024-09-25 18:59:55 +0300

    'frontend_mainpage'

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,16 +1,49 @@
 import './App.css';
-import { Navigate, Route, Routes } from "react-router-dom";
+import React from "react";
+import {useState, useEffect} from "react";
+import { Navigate, Route, Routes, BrowserRouter } from "react-router-dom";
 import Header from "./components/header/Header"
+import {Main} from "./components/main/Main"
 
 function App() {
+  const [announcements, setAnnouncements] = useState([])
+useEffect(
+  () => {
+    fetch('http://127.0.0.1:8000/api/?format=json')
+      .then((response) => response.json())
+      .then((data) => {
+        setAnnouncements(data);
+      });
+  },
+[]);
+
+
   return (
     <div className="App">
       <Header />
+      
 
+      <div className="MainList">
+        <ul>
+          {announcements.map((a) => {
+            return (
+            <div>
+              <li key={a.id}>{a.title}</li>
+              <li key={a.id}>{a.an_time}</li>
+              <li key={a.id}>{a.text}</li>
+            </div>
+            );
+          }
+          )}
+        </ul>
+      </div>
+
+
+      <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
+      </BrowserRouter>
 
     </div>
   );
--- project/frontend/src/components/main/Main.jsx
+++ project/frontend/src/components/main/Main.jsx
@@ -1,2 +1,4 @@
 import React from "react";
+import {useState, useEffect} from "react";
 import "./Main.css";
+
