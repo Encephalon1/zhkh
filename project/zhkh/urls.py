464c6d0f1ddd76c1312d1da5ab230d0ba69beecd
Encephalon1 <viizdevaetes@mail.ru>
2024-10-05 16:27:03 +0300

    'DRFsimpleJWT'

--- project/zhkh/urls.py
+++ project/zhkh/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .views import *
 
 
 urlpatterns = [
-    path('api/', AnnouncementListAPI.as_view(), name='api')
+    path('api/', AnnouncementListAPI.as_view(), name='api'),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
