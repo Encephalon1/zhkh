cfc0d08d78364833e17d618c0799847b5a320abe
Encephalon1 <viizdevaetes@mail.ru>
2025-02-15 06:50:53 +0300

    logout

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -5,6 +5,7 @@ import { Navigate, Route, Routes, BrowserRouter, Router } from "react-router-dom
 import Header from "./components/header/Header"
 import {Main} from "./components/main/Main"
 import Login from './components/auth/Login';
+import Logout from './components/auth/Logout';
 import AnnouncementCreateUpdate from './components/AnnouncementCreateUpdate';
 
 function App() {
@@ -43,6 +44,7 @@ useEffect(
       <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
+        <Route path='/logout' element={<Logout />} />
         <Route path='/announcement/' element={<AnnouncementCreateUpdate/>} />
         <Route path='/announcement/:pk' element={<AnnouncementCreateUpdate/>} />
         <Route path="*" element={<Navigate to="/" />} />
--- project/frontend/src/components/auth/Logout.js
+++ project/frontend/src/components/auth/Logout.js
@@ -1,1 +1,14 @@
 import React, { useState } from 'react';
+
+const Logout = () => {
+    const logoutUser = () => {
+      localStorage.removeItem('access');
+      localStorage.removeItem('refresh');
+    };
+  
+    return (
+      <button onClick={logoutUser}>Выход</button>
+    );
+  };
+  
+  export default Logout;
